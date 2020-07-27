fd295204b1f90cc58e742b4f1bbcc3f97359f385
dht2410 <dht2410@163.com>
2020-07-27 10:33:03 +0800

    some tests

---
+++ src/Test/StringTest.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String s = "vds";
+        char[] charArray = s.toCharArray();
+        System.out.println(s.equals(charArray));
+    }
+}
